14586ccbaab69876d87f752e324263cab7e52d41
JerryHu321 <27HuZ@albanyacademy.org>
2026-09-23 16:08:41 +0000

    final

--- Lab1.java
+++ Lab1.java
@@ -1,12 +1,17 @@
-/* Enter your header here
- * 
- * 
+/* Jerry Hu
+  Lab 1 – Beginning program to practice using print and println statements 
+  9/23/26
  */
 
 
 public class Lab1 {
     public static void main(String[] args) {
        //Enter your code here:
-       
+      System.out.print("I am sam. ");
+      System.out.print("Sam I am. ");
+      System.out.print("I do not like them, ");
+      System.out.println("Sam-I-am.");
+      System.out.println("I do not like green eggs and ham.");
+      
    }
 }
